8363ba4e6d3c8f8c419afa5d59727a6610387731
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-12-06 21:45:46 +1000

    Remove irrelevant data from each line

--- loganalyser.py
+++ loganalyser.py
@@ -7,19 +7,24 @@ Log Analyser
 
 def main():
     filename = "programming-task-example-data.log"
-    load_log_file(filename)
+    logs = load_log_file(filename)
+
+    for log in logs:
+        log_data = log.split("\"-\"", 1)[0]
 
 
 # Function to load the data into from the log file
 def load_log_file(filename):
+
+    # Try/except to check if the file can be loaded correctly
     try:
         with open(filename) as file_logs:
             file_logs = file_logs.readlines()
     except OSError as e:
         print("Could not open or read file: {}\n{}".format(filename, e))
+    # Catch all/different exceptions could be placed here for better error handling
 
-    for line in file_logs:
-        print(line, end='')
+    return file_logs
 
 
 main()
