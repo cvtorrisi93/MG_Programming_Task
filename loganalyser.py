9a3d5a79e15815ed75f34e33af83c959053aa536
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-12-07 20:25:28 +1000

    Consolidate get functions into gather_data function. Create get_info function

--- loganalyser.py
+++ loganalyser.py
@@ -8,39 +8,39 @@ Log Analyser
 def main():
     filename = "programming-task-example-data.log"
     logs = load_log_file(filename)
-    visited_urls = get_urls(logs)
-    ip_addresses = get_ip_addresses(logs)
+    data = gather_data(logs)
+    print(data)
 
 
-# Function to get all of the IP Addresses that visited each URL
-# and count the number of times they occurred
-def get_ip_addresses(logs):
-    all_ip_addresses = {}
+# Gather all relevant data for analysis into an array
+# The first element will contain an array of IP addresses
+# The second array will contain an array of the visited URLs
+def gather_data(logs):
+
+    all_ip_addresses, all_urls = {}, {}
     for log in logs:
-        # Split method below is under the assumption that all log files are formatted in this way.
+        # Split methods below is under the assumption that all log files are formatted in a consistent way.
         ip_address = log.split(" ")[0]
-        if ip_address in all_ip_addresses:
-            all_ip_addresses[ip_address] += 1  # Add one IP address to an existing dictionary item (IP Address)
-        else:
-            all_ip_addresses[ip_address] = 1  # Create new dictionary item for "new" IP address
-    # Sort from highest to lowest (number of occurrences)
-    return sorted(all_ip_addresses.items(), key=lambda x: x[1], reverse=True)
-
-
-# Function to get all urls that were visited in the logged data
-# and count each time the site was visited
-def get_urls(logs):
-
-    all_urls = {}
-    for log in logs:
-        # Split method below is under the assumption that all log files are formatted in this way.
         url = log.split("\"-\"", 1)[0].split("GET ", 1)[1].split(" ")[0]
-        if url in all_urls:
-            all_urls[url] += 1  # Add one visit to an existing dictionary item (url)
-        else:
-            all_urls[url] = 1  # Create new dictionary item for "new" url
-    # Sort from highest to lowest (number of occurrences)
-    return sorted(all_urls.items(), key=lambda x: x[1], reverse=True)
+
+        all_ip_addresses = get_info(all_ip_addresses, ip_address)
+
+        all_urls = get_info(all_urls, url)
+
+    # Sort from highest to lowest based on value of each key (URL or IP Address occurrences)
+    all_ip_addresses = sorted(all_ip_addresses.items(), key=lambda x: x[1], reverse=True)
+    all_urls = sorted(all_urls.items(), key=lambda x: x[1], reverse=True)
+    return [all_ip_addresses, all_urls]
+
+
+# Get a stripped item, add to or check if it is an existing item in the dictionary
+def get_info(item_dict, item):
+    if item in item_dict:
+        item_dict[item] += 1  # Add one to the value of an existing dictionary item
+    else:
+        item_dict[item] = 1  # Create new dictionary item
+
+    return item_dict
 
 
 # Function to load the data from the log file
